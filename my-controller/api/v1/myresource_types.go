0485c622c4cdc33a28dce79bd5b3c50e7154cf5b
Mahdi Bouaziz <bouazizmedmahdi@gmail.com>
2025-01-30 15:17:11 +0100

    added api types

---
+++ my-controller/api/v1/myresource_types.go
@@ -0,0 +1,61 @@
+/*
+Copyright 2025.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// MyResourceSpec defines the desired state of MyResource.
+type MyResourceSpec struct {
+	// Important: Run "make" to regenerate code after modifying this file
+	Replicas int32  `json:"replicas"`
+	Image    string `json:"image"`
+}
+
+// MyResourceStatus defines the observed state of MyResource.
+type MyResourceStatus struct {
+	// Important: Run "make" to regenerate code after modifying this file
+	AvailableReplicas int32 `json:"availableReplicas"`
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+
+// MyResource is the Schema for the myresources API.
+type MyResource struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   MyResourceSpec   `json:"spec,omitempty"`
+	Status MyResourceStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// MyResourceList contains a list of MyResource.
+type MyResourceList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []MyResource `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&MyResource{}, &MyResourceList{})
+}
